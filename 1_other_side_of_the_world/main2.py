1a8b7501fabaa86ac5200f273873072250604499
Nathan <nathanjwtx@nathanjwtx.io>
2021-02-05 08:15:57 -0600

    began adding map icons and formatting

--- 1_other_side_of_the_world/main2.py
+++ 1_other_side_of_the_world/main2.py
@@ -6,9 +6,21 @@ longitude = 67.032592
 
 mymap = Map(location=[latitude, longitude])
 
+popup_content = """
+12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
+<hr style="margin:1px">
+12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
+<hr style="margin:1px">
+12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
+<hr style="margin:1px">
+12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
+<hr style="margin:1px">
+"""
+
 geopoint = Geopoint(latitude, longitude)
 
-popup = Popup(str(geopoint.get_weather()[0]))
+# popup = Popup(str(geopoint.get_weather()[0]))
+popup = Popup(popup_content, max_width=400)
 popup.add_to(geopoint)
 
 geopoint.add_to(mymap)
