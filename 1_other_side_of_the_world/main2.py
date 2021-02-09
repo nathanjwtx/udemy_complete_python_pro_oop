fb9aab34f2623b13c03ec47f3fc419c79c9c1c47
Nathan <nathanjwtx@nathanjwtx.io>
2021-02-09 06:53:14 -0600

    first commit with pre-commit

--- 1_other_side_of_the_world/main2.py
+++ 1_other_side_of_the_world/main2.py
@@ -1,23 +1,31 @@
 from folium import Map, Popup
 from geo import Geopoint
 
-latitude = 24.850134
-longitude = 67.032592
+# karachi
+# latitude = 24.850134
+# longitude = 67.032592
+
+# wylie
+latitude = 32.98226
+longitude = -96.53057
 
 mymap = Map(location=[latitude, longitude])
 
-popup_content = """
-12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
-<hr style="margin:1px">
-12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
-<hr style="margin:1px">
-12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
-<hr style="margin:1px">
-12: 70°F <img src="http://openweathermap.org/img/wn/10d@2x.png" width="35">
-<hr style="margin:1px">
-"""
 
 geopoint = Geopoint(latitude, longitude)
+
+forecast = geopoint.get_weather()
+
+popup_content = f"""
+{forecast[0][0][-8:-6]}: {round(forecast[0][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[1][0][-8:-6]}: {round(forecast[1][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[2][0][-8:-6]}: {round(forecast[2][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+{forecast[3][0][-8:-6]}: {round(forecast[3][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width="35">
+<hr style="margin:1px">
+"""
 
 # popup = Popup(str(geopoint.get_weather()[0]))
 popup = Popup(popup_content, max_width=400)
