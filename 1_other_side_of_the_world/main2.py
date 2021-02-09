bde8806a83ffa728dac5c0f150c7a4f2b91d8d90
Nathan <nathanjwtx@nathanjwtx.io>
2021-02-09 07:18:27 -0600

    add a for loop for multiple locations

--- 1_other_side_of_the_world/main2.py
+++ 1_other_side_of_the_world/main2.py
@@ -6,31 +6,33 @@ from geo import Geopoint
 # longitude = 67.032592
 
 # wylie
-latitude = 32.98226
-longitude = -96.53057
+# latitude = 32.98226
+# longitude = -96.53057
 
-mymap = Map(location=[latitude, longitude])
+locations = [[24.850134, 67.032592], [32.98226, -96.53057], [50.79052100216682, -1.0915188523036652]]
 
+mymap = Map(location=[50.79052100216682, -1.0915188523036652])
 
-geopoint = Geopoint(latitude, longitude)
+for loc in locations:
+    geopoint = Geopoint(latitude=loc[0], longitude=loc[1])
 
-forecast = geopoint.get_weather()
+    forecast = geopoint.get_weather()
 
-popup_content = f"""
-{forecast[0][0][-8:-6]}: {round(forecast[0][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width="35">
-<hr style="margin:1px">
-{forecast[1][0][-8:-6]}: {round(forecast[1][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width="35">
-<hr style="margin:1px">
-{forecast[2][0][-8:-6]}: {round(forecast[2][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width="35">
-<hr style="margin:1px">
-{forecast[3][0][-8:-6]}: {round(forecast[3][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width="35">
-<hr style="margin:1px">
-"""
+    popup_content = f"""
+    {forecast[0][0][-8:-6]}: {round(forecast[0][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[0][-1]}@2x.png" width="35">
+    <hr style="margin:1px">
+    {forecast[1][0][-8:-6]}: {round(forecast[1][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[1][-1]}@2x.png" width="35">
+    <hr style="margin:1px">
+    {forecast[2][0][-8:-6]}: {round(forecast[2][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[2][-1]}@2x.png" width="35">
+    <hr style="margin:1px">
+    {forecast[3][0][-8:-6]}: {round(forecast[3][1])}°F <img src="http://openweathermap.org/img/wn/{forecast[3][-1]}@2x.png" width="35">
+    <hr style="margin:1px">
+    """
 
-# popup = Popup(str(geopoint.get_weather()[0]))
-popup = Popup(popup_content, max_width=400)
-popup.add_to(geopoint)
+    # popup = Popup(str(geopoint.get_weather()[0]))
+    popup = Popup(popup_content, max_width=400)
+    popup.add_to(geopoint)
 
-geopoint.add_to(mymap)
+    geopoint.add_to(mymap)
 
 mymap.save('map.html')
